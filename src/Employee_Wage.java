023033287a6b2b18015a57a62e96889168bc0a74
nageshwadkar2001 <nageshwadkar25@gmail.com>
2024-05-30 16:21:24 +0530

    uc9_Total_For_Each_Company_Using_instaance_variable_add

--- src/Employee_Wage.java
+++ src/Employee_Wage.java
@@ -1,46 +1,66 @@
-public class Employee_Wage {
+public  class Employee_Wage{
 
     int FULL_TIME_EMP = 1;
     int PART_TIME_EMP = 2;
     int EMP_FULLTIME_WORK_HOUR = 8;
     int EMP_PARTTIME_WORK_HOUR = 4;
+    int total_wage = 0;
+    String company;
+    int EMP_RATE_PER_HOUR;
+    int EMP_WORK_DAYS;
+    int EMP_WORK_HOUR;
+
+    public Employee_Wage(String company, int EMP_RATE_PER_HOUR, int EMP_WORK_DAYS, int EMP_WORK_HOUR){
+
+        this.company = company;
+        this.EMP_RATE_PER_HOUR = EMP_RATE_PER_HOUR;
+        this.EMP_WORK_DAYS = EMP_WORK_DAYS;
+        this.EMP_WORK_HOUR = EMP_WORK_HOUR;
+    }
 
 
-    public int calculateEmployeeWage(String company, int EMP_RATE_PER_HOUR, int EMP_WORK_DAYS, int EMP_WORK_HOUR) {
+    public int calculateEmployeeWage(){
 
         int emp_wage = 0;
-        int total_wage = 0;
         int total_emp_work_hour = 0;
         int total_emp_work_day = 0;
 
-        while (total_emp_work_hour <= EMP_WORK_HOUR && total_emp_work_day < EMP_WORK_DAYS) {
+        while(total_emp_work_hour<=EMP_WORK_HOUR && total_emp_work_day<EMP_WORK_DAYS){
 
-            total_emp_work_day++;
-            int emp_check = (int) Math.floor(Math.random() * 10 % 3);
+            total_emp_work_day ++;
+            int emp_check = (int)Math.floor(Math.random()*10%3);
 
-            if (emp_check == FULL_TIME_EMP) {
+            if(emp_check == FULL_TIME_EMP){
                 emp_wage = EMP_RATE_PER_HOUR * EMP_FULLTIME_WORK_HOUR;
                 total_emp_work_hour += EMP_FULLTIME_WORK_HOUR;
-            } else if (emp_check == PART_TIME_EMP) {
+            }
+            else if(emp_check == PART_TIME_EMP){
                 emp_wage = EMP_RATE_PER_HOUR * EMP_PARTTIME_WORK_HOUR;
                 total_emp_work_hour += EMP_PARTTIME_WORK_HOUR;
-            } else {
+            }
+            else{
                 emp_wage = 0;
                 total_emp_work_hour += 0;
             }
             total_wage += emp_wage;
         }
-        System.out.println("Total wages of Company is : ");
         return total_wage;
     }
 
-    public static void main(String[] args) {
-//Working
-        Employee_Wage emp1 = new Employee_Wage();
-        System.out.println(emp1.calculateEmployeeWage("Nokia", 400, 25, 150));
-
-        Employee_Wage emp2 = new Employee_Wage();
-        System.out.println(emp2.calculateEmployeeWage("Apple", 600, 27, 162));
+    //@Override
+    public String toString(){
+        return "Total Employee Wage for comapny " +company + " is : " + total_wage;
     }
 
+    public static void main(String[] args) {
+
+        Employee_Wage emp1 = new Employee_Wage("Nokia", 400, 25, 150);
+        emp1.calculateEmployeeWage();
+        System.out.println(emp1);
+
+        Employee_Wage emp2 = new Employee_Wage("Apple", 600, 27, 162);
+        emp2.calculateEmployeeWage();
+        System.out.println(emp2);
+
+    }
 }
